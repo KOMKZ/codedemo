0d48f9bd63f5f13c0618cc610472508ef3911a6a
kz <784248377@qq.com>
2017-10-30 13:01:18 +0800

    提交学习代码

---
+++ vue-pro/hello-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+	<div id="#app">
+		<p>hello world</p>
+	</div>
+</template>
+
+<script>
+let routes, components = {
+	index : {template : "<p>hello world</p>"},
+	contact : {template : "<p>contact</p>"},
+	detail : {template : "<p>detail</p>"},
+	not_found : {template : "<p>not found</p>"}
+}
+export default {
+	name : "app",
+
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ vue-pro/w-b-js/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+	entry: [
+		"babel-polyfill",
+		"./src/index.js"
+	],
+	output: {
+		path: __dirname + '/output/',
+		publicPath: "/output/",
+		filename: 'index.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
+};
